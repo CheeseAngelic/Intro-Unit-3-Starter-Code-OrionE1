e9bf01a6c31695beb700fa5d20a4bf58a30d4580
CheeseAngelic <131998476+CheeseAngelic@users.noreply.github.com>
2023-04-27 16:48:26 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,78 @@
+let sleep = document.querySelector(".option-one")
+let sleepscreen = document.querySelector(".option-one-screen")
+let kitchen = document.querySelector(".option-two")
+let kitchenscreen = document.querySelector(".option-two-screen")
+let ignoresound = document.querySelector(".option-three")
+let ignoresoundscreen = document.querySelector(".option-three-screen")
+let yoursoul = document.querySelector(".option-four")
+let yoursoulscreen = document.querySelector(".option-four-screen")
+let frontofstore = document.querySelector(".option-five")
+let frontofstorescreen = document.querySelector(".option-five-screen")
+let backofstore = document.querySelector(".option-six")
+let backofstorescreen = document.querySelector(".option-six-screen")
+let killjimbo = document.querySelector(".option-seven")
+let killjimboscreen = document.querySelector(".option-seven-screen")
+let dontkilljimbo = document.querySelector(".option-eight")
+let dontkilljimbocreen = document.querySelector(".option-eight-screen")
+let walkbackwards = document.querySelector(".option-nine")
+let walkbackwardsscreen = document.querySelector(".option-nine-screen")
+let walkfoward = document.querySelector(".option-ten")
+let walkfowardscreen = document.querySelector(".option-ten-screen")
+let optiononeend = document.querySelector(".option-one-end")
+let optiononeendscreen = document.querySelector(".option-one-screen")
+let optiontwoend = document.querySelector(".option-two-end")
+let optiontwoendscreen = document.querySelector(".option-two-screen")
+
+sleep.onclick = function(){
+sleepscreen.style.display = "block";
+};
+
+kitchen.onclick = function(){
+kitchenscreen.style.display = "block";
+};
+
+ignoresound.onclick = function(){
+ignoresoundscreen.style.display = "block";
+};
+
+yoursoul.onclick = function(){
+yoursoulscreen.style.display = "block";
+  body.style.backgroundcolor = "black"
+  body.style.color = "red" 
+};
+
+frontofstore.onclick = function(){
+frontofstorescreen.style.display = "block";
+};
+
+backofstore.onclick = function(){
+backofstorescreen.style.display = "block";
+};
+
+killjimbo.onclick = function(){
+killjimboscreen.style.display = "block";
+};
+
+dontkilljimbo.onclick = function(){
+dontkilljimboscreen.style.display = "block";
+};
+
+walkbackwards.onclick = function(){
+walkbackwardsscreen.style.display = "block";
+};
+
+walkfoward.onclick = function(){
+walkfowardscreen.style.display = "block";
+};
+
+optiononeend.onclick = function(){
+optiononeendscreen.style.display = "block";
+};
+
+optiononeend.onclick = function(){
+optiononeendscreen.style.display = "block";
+};
+
+optiontwoend.onclick = function(){
+optiontwoendscreen.style.display = "block";
+};
